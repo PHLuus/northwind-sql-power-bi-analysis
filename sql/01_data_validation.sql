fce17e83d46dba6ef14f28545e16d3cd7b62f8e7
PHLuus <hugoluus@gmail.com>
2026-07-02 10:37:18 +0200

    Create 01_data_validation.sql

---
+++ sql/01_data_validation.sql
@@ -0,0 +1,4 @@
+SELECT COUNT(*) AS CustomerCount FROM Customers;
+SELECT COUNT(*) AS OrderCount FROM Orders;
+SELECT COUNT(*) AS ODCount FROM [Order Details];
+SELECT COUNT(*) AS PruductCount FROM Products;
